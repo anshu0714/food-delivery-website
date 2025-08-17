266f57ddcbb0467bc261d60051fcec13209a3e3d
anshu0714 <anshujha14072002@gmail.com>
2025-08-17 23:59:54 +0530

    Created first functional react component

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Namaste React!</h1>
+      <p>Welcome to the episode 4 of the series.</p>
+    </div>
+  );
+}
