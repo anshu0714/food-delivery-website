6de16c70a7a0debc6d2114ce71e043ef0862eb29
anshu0714 <anshujha14072002@gmail.com>
2025-09-01 23:50:45 +0530

    Update styling of hero section and add mock restaurant dataset for cards

--- App.js
+++ App.js
@@ -53,19 +53,21 @@ const Hero = () => {
       </div>
       <div className="hero-left">
         <div className="review">
-          <h4>Order</h4>
-          <p>We’ve Received your order!</p>
-          <p>Awaiting Restaurant acceptance </p>
+          <h4>Review</h4>
+          <p>John D.</p>
+          <p>“Great food and quick delivery!”</p>
         </div>
+
         <div className="review">
-          <h4>Order</h4>
-          <p>We’ve Received your order!</p>
-          <p>Awaiting Restaurant acceptance </p>
+          <h4>Review</h4>
+          <p>Sarah K.</p>
+          <p>“Loved the flavors, will order again.”</p>
         </div>
+
         <div className="review">
-          <h4>Order</h4>
-          <p>We’ve Received your order!</p>
-          <p>Awaiting Restaurant acceptance </p>
+          <h4>Review</h4>
+          <p>Michael P.</p>
+          <p>“Packaging was neat and food was hot!”</p>
         </div>
       </div>
     </div>
@@ -78,40 +80,173 @@ const Body = () => {
       <div className="search-bar">search</div>
       {/* Restaurant Card */}
       <div className="restaurant-container">
-        <RestaurantCard
-          resName="Meghana Foods"
-          cuisine="Biryani, North India, Asian"
-          star="4.4"
-          time="38"
-        />
-        <RestaurantCard
-          resName="KFC"
-          cuisine="Burgers, Fast Food"
-          star="4.6"
-          time="20"
-        />
+        <div className="restaurant-container">
+          {resList.map((restaurant, index) => (
+            <RestaurantCard key={index} resData={restaurant} />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
+const resList = [
+  {
+    resName: "KFC",
+    cuisine: "Burgers, Fast Food",
+    star: "4.6",
+    time: "20",
+    image: "https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg"
+  },
+  {
+    resName: "McDonald's",
+    cuisine: "Burgers, Beverages",
+    star: "4.4",
+    time: "18",
+    image: "https://images.pexels.com/photos/3020919/pexels-photo-3020919.jpeg"
+  },
+  {
+    resName: "Domino's Pizza",
+    cuisine: "Pizza, Fast Food",
+    star: "4.2",
+    time: "25",
+    image: "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg"
+  },
+  {
+    resName: "Subway",
+    cuisine: "Healthy Food, Sandwiches",
+    star: "4.3",
+    time: "22",
+    image: "https://images.pexels.com/photos/1647163/pexels-photo-1647163.jpeg"
+  },
+  {
+    resName: "Burger King",
+    cuisine: "Burgers, Beverages",
+    star: "4.1",
+    time: "19",
+    image: "https://images.pexels.com/photos/1556698/pexels-photo-1556698.jpeg"
+  },
+  {
+    resName: "Pizza Hut",
+    cuisine: "Pizza, Fast Food",
+    star: "4.0",
+    time: "24",
+    image: "https://images.pexels.com/photos/367915/pexels-photo-367915.jpeg"
+  },
+  {
+    resName: "Starbucks",
+    cuisine: "Coffee, Beverages",
+    star: "4.7",
+    time: "15",
+    image: "https://b.zmtcdn.com/data/pictures/chains/8/35008/703ffbb5de67e078d3f7df6f2a6a07b3.jpg"
+  },
+  {
+    resName: "Taco Bell",
+    cuisine: "Mexican, Fast Food",
+    star: "4.2",
+    time: "23",
+    image: "https://b.zmtcdn.com/data/pictures/chains/2/35002/77bfe8b116b0ed0dca2b1fe97958a3d3.jpg"
+  },
+  {
+    resName: "Haldiram's",
+    cuisine: "Indian, Sweets",
+    star: "4.5",
+    time: "20",
+    image: "https://b.zmtcdn.com/data/pictures/chains/1/35001/63559b2bb2c7087f3aa1e50ab6966482.jpg"
+  },
+  {
+    resName: "Bikanervala",
+    cuisine: "North Indian, Mithai",
+    star: "4.3",
+    time: "21",
+    image: "https://b.zmtcdn.com/data/pictures/chains/9/35009/7c939e0d4e2f43fa43a5b775d390d05e.jpg"
+  },
+  {
+    resName: "Cafe Coffee Day",
+    cuisine: "Coffee, Snacks",
+    star: "4.0",
+    time: "17",
+    image: "https://b.zmtcdn.com/data/pictures/chains/8/35008/322d39ac787f63ff12aeb97a39f3b6d2.jpg"
+  },
+  {
+    resName: "Barbeque Nation",
+    cuisine: "BBQ, North Indian",
+    star: "4.6",
+    time: "30",
+    image: "https://b.zmtcdn.com/data/pictures/chains/9/35009/9c92f82ac6a9b9a53f50857c7e4f4c6b.jpg"
+  },
+  {
+    resName: "Wow! Momo",
+    cuisine: "Momos, Fast Food",
+    star: "4.2",
+    time: "16",
+    image: "https://b.zmtcdn.com/data/pictures/chains/3/35003/d3b59d62ef5f32d6cf44e2d328d7e109.jpg"
+  },
+  {
+    resName: "Faasos",
+    cuisine: "Rolls, North Indian",
+    star: "4.4",
+    time: "22",
+    image: "https://b.zmtcdn.com/data/pictures/chains/6/35006/b8b26cbcb814a478b0b0a6d8a42a996a.jpg"
+  },
+  {
+    resName: "Behrouz Biryani",
+    cuisine: "Biryani, Mughlai",
+    star: "4.6",
+    time: "28",
+    image: "https://b.zmtcdn.com/data/pictures/chains/4/35004/00b481c2c8a3543b2adbc2c2c2b45fd5.jpg"
+  },
+  {
+    resName: "FreshMenu",
+    cuisine: "Healthy Food, Salads",
+    star: "4.1",
+    time: "18",
+    image: "https://b.zmtcdn.com/data/pictures/chains/5/35005/19787c62eab4167b5a9f878c95628a33.jpg"
+  },
+  {
+    resName: "Chai Point",
+    cuisine: "Tea, Snacks",
+    star: "4.3",
+    time: "14",
+    image: "https://b.zmtcdn.com/data/pictures/chains/6/35006/9db8712f2e7c8d52efedec4f3c9f2af4.jpg"
+  },
+  {
+    resName: "The Belgian Waffle Co.",
+    cuisine: "Desserts, Waffles",
+    star: "4.7",
+    time: "19",
+    image: "https://b.zmtcdn.com/data/pictures/chains/7/35007/8c5b57f871f2b791a2ebd938c74a0e2d.jpg"
+  },
+  {
+    resName: "Chinese Wok",
+    cuisine: "Chinese, Asian",
+    star: "4.2",
+    time: "26",
+    image: "https://b.zmtcdn.com/data/pictures/chains/8/35008/5e86b6df8d8473f88cf8e9a6c29a2bbf.jpg"
+  },
+  {
+    resName: "Giani's",
+    cuisine: "Ice Cream, Desserts",
+    star: "4.5",
+    time: "12",
+    image: "https://b.zmtcdn.com/data/pictures/chains/9/35009/2b0d3f4f9a7eeb2ef2d1bdf4c6f7bc77.jpg"
+  }
+];
 
-const RestaurantCard = (props) => {
-  // Destructuring props object
-  const { resName, cuisine, star, time } = props;
 
+const RestaurantCard = ({ resData }) => {
   return (
     <div className="restaurant-card">
       <img
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2025/6/6/a009bdbf-d06c-4342-820c-f9ac37ab58a4_e21acc8c-dc4a-4177-b5e6-f4fc1ee2251e.jpg"
-        alt="res-logo"
+        src={resData.image}
+        alt={resData.resName}
         className="res-logo"
       />
       {/* Without destructuring we use it like this */}
       {/* <h1>{props.resName}</h1> */}
-      <h1>{resName}</h1>
-      <h4>{cuisine}</h4>
-      <h4>{star} stars</h4>
-      <h4>{time} minutes</h4>
+      <h1>{resData.resName}</h1>
+      <h4>{resData.cuisine}</h4>
+      <h4>{resData.star} stars</h4>
+      <h4>{resData.time} minutes</h4>
     </div>
   );
 };
