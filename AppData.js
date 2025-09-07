726cdcbf76f7889275c00174541c6deda62d7373
anshu0714 <anshujha14072002@gmail.com>
2025-09-07 22:02:55 +0530

    Add popular categories data in the list

--- AppData.js
+++ AppData.js
@@ -179,5 +179,30 @@ export const popularCategories = [
     image: "https://images.pexels.com/photos/551991/pexels-photo-551991.jpeg",
     category: "Burgers and Fast Food",
     number: "4",
+  },
+  {
+    image: "https://images.pexels.com/photos/30472961/pexels-photo-30472961.jpeg",
+    category: "Salads",
+    number: "24",
+  },
+  {
+    image: "https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg",
+    category: "Pasta and Casuals",
+    number: "32",
+  },
+  {
+    image: "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg",
+    category: "Pizzas",
+    number: "10",
+  },
+  {
+    image: "https://images.pexels.com/photos/2113556/pexels-photo-2113556.jpeg",
+    category: "Breakfast",
+    number: "5",
+  },
+  {
+    image: "https://images.pexels.com/photos/209540/pexels-photo-209540.jpeg",
+    category: "Soups",
+    number: "14",
   }
 ];
