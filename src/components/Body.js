f89076eb739c4c02958da95c03422b07eb75abc0
anshu0714 <anshujha14072002@gmail.com>
2025-09-08 23:38:36 +0530

    Add the useState hook to change state of restaurant cards

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,43 +8,62 @@ import OfferToggle from "./OfferToggle";
 import PopularCategoriesCard from "./PopularCategoriesCard";
 import PopularRestaurantsCard from "./PopularRestaurantsCard";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
-const Body = () => (
-  <div className="body">
-    <Hero />
-    <div className="offers-section">
-      <div className="offer-heading">
-        <h2>Up to -40% 🎊 Exclusive deals</h2>
-        <OfferToggle />
+const Body = () => {
+  // State Variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  return (
+    <div className="body">
+      <Hero />
+      <div className="offers-section">
+        <div className="offer-heading">
+          <h2>Up to -40% 🎊 Exclusive deals</h2>
+          <OfferToggle />
+        </div>
+        <div className="offers-container">
+          {offersList.map((offer) => (
+            <OffersCard key={offer.id} offers={offer} />
+          ))}
+        </div>
       </div>
-      <div className="offers-container">
-        {offersList.map((offer) => (
-          <OffersCard key={offer.id} offers={offer} />
+      <div className="popular-categories">
+        <h2>Popular Categories 🤩</h2>
+        <div className="popular-container">
+          {popularCategories.map((popular) => (
+            <PopularCategoriesCard key={popular.id} popular={popular} />
+          ))}
+        </div>
+      </div>
+      <div className="popular-restaurants">
+        <h2>Popular Restaurants</h2>
+        <div className="popular-res-container">
+          {popularRestaurants.map((popular) => (
+            <PopularRestaurantsCard key={popular.id} popular={popular} />
+          ))}
+        </div>
+      </div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          type="button"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.star > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="restaurant-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
     </div>
-    <div className="popular-categories">
-      <h2>Popular Categories 🤩</h2>
-      <div className="popular-container">
-        {popularCategories.map((popular) => (
-          <PopularCategoriesCard key={popular.id} popular={popular} />
-        ))}
-      </div>
-    </div>
-    <div className="popular-restaurants">
-      <h2>Popular Restaurants</h2>
-      <div className="popular-res-container">
-        {popularRestaurants.map((popular) => (
-          <PopularRestaurantsCard key={popular.id} popular={popular} />
-        ))}
-      </div>
-    </div>
-    <div className="restaurant-container">
-      {resList.map((restaurant) => (
-        <RestaurantCard key={restaurant.id} resData={restaurant} />
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 export default Body;
