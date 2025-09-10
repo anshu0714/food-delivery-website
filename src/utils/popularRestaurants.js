1e623bed04c8e065b2bd8e0116cabfb6bfe71f32
anshu0714 <anshujha14072002@gmail.com>
2025-09-10 22:04:37 +0530

    Add the popular restaurants items

--- src/utils/popularRestaurants.js
+++ src/utils/popularRestaurants.js
@@ -5,6 +5,31 @@ const popularRestaurants = [
     image:
       "https://images.unsplash.com/photo-1602400236316-f5e3b6d2314c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWNkb25hbGRzfGVufDB8fDB8fHww",
   },
+  {
+    id: 2,
+    name: "KFC",
+    image: "https://images.unsplash.com/photo-1637851682487-fa13fce5150f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8a2ZjfGVufDB8fDB8fHww",
+  },
+  {
+    id: 3,
+    name: "Subway",
+    image: "https://images.unsplash.com/photo-1644625143311-9510da7614a1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8U1VCV0FZJTIwRk9PRHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    id: 4,
+    name: "Burger King",
+    image: "https://images.unsplash.com/photo-1587476353394-d031d13188ff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YnVyZ2VyJTIwa2luZ3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    id: 5,
+    name: "Domino's Pizza",
+    image: "https://images.unsplash.com/photo-1695920042189-3932267e2421?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    name: "Pizzeria",
+    image: "https://plus.unsplash.com/premium_photo-1675884330914-4f91255343a6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMyfHxyZXN0YXVyYW50JTIwbmFtZXxlbnwwfHwwfHx8MA%3D%3D",
+  },
 ];
 
 export default popularRestaurants;
