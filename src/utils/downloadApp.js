6ed838a62bceea613f7e9fb8142b2468500e2942
anshu0714 <anshujha14072002@gmail.com>
2025-09-11 21:29:15 +0530

    Add the dowload details of the platform

---
+++ src/utils/downloadApp.js
@@ -0,0 +1,18 @@
+import { APPLE_PLAY_STORE_ICON, GOOGLE_PLAY_STORE_ICON } from "./constant";
+
+const downloadApp = [
+  {
+    id: 1,
+    name: "Google Play",
+    tagline: "Get it on",
+    image: { GOOGLE_PLAY_STORE_ICON },
+  },
+  {
+    id: 2,
+    name: "App Store",
+    tagline: "Download on the",
+    image: { APPLE_PLAY_STORE_ICON },
+  },
+];
+
+export default downloadApp;
