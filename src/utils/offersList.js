0700f518704394672fbd5a84408a2f711c11dff7
anshu0714 <anshujha14072002@gmail.com>
2025-09-14 21:32:24 +0530

    Add more items in offers list

--- src/utils/offersList.js
+++ src/utils/offersList.js
@@ -1,4 +1,5 @@
 const offersList = [
+  // Chicken
   {
     id: 1,
     name: "KFC",
@@ -9,14 +10,50 @@ const offersList = [
   },
   {
     id: 2,
+    name: "Nando's",
+    category: "Restaurant",
+    offer: "25%",
+    type: "Chicken",
+    image: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
+  },
+  {
+    id: 3,
+    name: "Biryani Blues",
+    category: "Restaurant",
+    offer: "15%",
+    type: "Chicken",
+    image: "https://images.pexels.com/photos/9609855/pexels-photo-9609855.jpeg",
+  },
+
+  // Coffee
+  {
+    id: 4,
     name: "Starbucks",
     category: "Cafe",
     offer: "15%",
     type: "Coffee",
+    image: "https://images.pexels.com/photos/4481974/pexels-photo-4481974.jpeg",
+  },
+  {
+    id: 5,
+    name: "Cafe Coffee Day",
+    category: "Cafe",
+    offer: "20%",
+    type: "Coffee",
     image: "https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg",
   },
   {
-    id: 3,
+    id: 6,
+    name: "Costa Coffee",
+    category: "Cafe",
+    offer: "10%",
+    type: "Coffee",
+    image: "https://images.pexels.com/photos/851555/pexels-photo-851555.jpeg",
+  },
+
+  // Pizza
+  {
+    id: 7,
     name: "Domino's Pizza",
     category: "Restaurant",
     offer: "25%",
@@ -24,21 +61,46 @@ const offersList = [
     image: "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg",
   },
   {
-    id: 4,
-    name: "Cafe Coffee Day",
-    category: "Cafe",
+    id: 8,
+    name: "Pizza Hut",
+    category: "Restaurant",
     offer: "20%",
-    type: "Coffee",
-    image: "https://images.pexels.com/photos/2067396/pexels-photo-2067396.jpeg",
+    type: "Pizza",
+    image: "https://images.pexels.com/photos/845811/pexels-photo-845811.jpeg",
   },
   {
-    id: 5,
+    id: 9,
+    name: "Papa John's",
+    category: "Restaurant",
+    offer: "15%",
+    type: "Pizza",
+    image: "https://images.pexels.com/photos/1435907/pexels-photo-1435907.jpeg",
+  },
+
+  // Dessert
+  {
+    id: 10,
     name: "Giani's",
     category: "Cafe",
-
     offer: "12%",
     type: "Dessert",
     image: "https://images.pexels.com/photos/1362534/pexels-photo-1362534.jpeg",
+  },
+  {
+    id: 11,
+    name: "The Sweet Boutique",
+    category: "Cafe",
+    offer: "18%",
+    type: "Dessert",
+    image: "https://images.pexels.com/photos/808941/pexels-photo-808941.jpeg",
+  },
+  {
+    id: 12,
+    name: "Frozen Treats",
+    category: "Cafe",
+    offer: "15%",
+    type: "Dessert",
+    image: "https://images.pexels.com/photos/461431/pexels-photo-461431.jpeg",
   },
 ];
 
