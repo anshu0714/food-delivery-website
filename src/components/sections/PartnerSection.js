aa8ac8cd1fda970da3a410c02e31a051741c16a7
anshu0714 <anshujha14072002@gmail.com>
2025-09-17 22:24:54 +0530

    Add the styling to the partner section

--- src/components/sections/PartnerSection.js
+++ src/components/sections/PartnerSection.js
@@ -3,19 +3,23 @@ const PartnerSection = () => {
     <div className="partner-container">
       <div className="partners-card card-partner">
         <p className="tag">Earn more with lower fees</p>
-        <p className="tagline">Signup as a business</p>
-        <h3 className="heading">Partner With Us</h3>
-        <button className="partner-button" type="button">
-          Get Started
-        </button>
+        <div className="content">
+          <p className="tagline">Signup as a business</p>
+          <h3 className="heading">Partner With Us</h3>
+          <button className="partner-button" type="button">
+            Get Started
+          </button>
+        </div>
       </div>
       <div className="partners-card card-ride">
-        <p className="tag">Avail exclusive pers</p>
-        <p className="tagline">Signup as a rider</p>
-        <h3 className="heading">Ride With Us</h3>
-        <button className="partner-button" type="button">
-          Get Started
-        </button>
+        <p className="tag">Avail exclusive perks</p>
+        <div className="content">
+          <p className="tagline">Signup as a rider</p>
+          <h3 className="heading">Ride With Us</h3>
+          <button className="partner-button" type="button">
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
   );
