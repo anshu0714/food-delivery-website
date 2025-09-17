c5618cf6f30bcb52542f63900fc354bcbe5c25a6
anshu0714 <anshujha14072002@gmail.com>
2025-09-17 22:39:45 +0530

    Create a key stat file

---
+++ src/utils/keyStats.js
@@ -0,0 +1,22 @@
+const keyStats = [
+  {
+    id: 1,
+    title: "Registered Riders",
+    value: "546+",
+  },
+  {
+    id: 2,
+    title: "Orders Delivered",
+    value: "789,900+",
+  },
+  {
+    id: 3,
+    title: "Restaurants Partnered",
+    value: "690+",
+  },
+  {
+    id: 4,
+    title: "Food items",
+    value: "17,457+",
+  },
+];
