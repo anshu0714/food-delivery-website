fb4ace8231fbeb43f066efa56e414f60f688b7a6
anshu0714 <anshujha14072002@gmail.com>
2025-09-17 22:32:00 +0530

    Make PartnerSection dynamic by mapping data from partnerList.js

--- src/components/sections/PartnerSection.js
+++ src/components/sections/PartnerSection.js
@@ -1,26 +1,25 @@
+import partnerList from "../../utils/partnerList";
+
 const PartnerSection = () => {
   return (
     <div className="partner-container">
-      <div className="partners-card card-partner">
-        <p className="tag">Earn more with lower fees</p>
-        <div className="content">
-          <p className="tagline">Signup as a business</p>
-          <h3 className="heading">Partner With Us</h3>
-          <button className="partner-button" type="button">
-            Get Started
-          </button>
-        </div>
-      </div>
-      <div className="partners-card card-ride">
-        <p className="tag">Avail exclusive perks</p>
-        <div className="content">
-          <p className="tagline">Signup as a rider</p>
-          <h3 className="heading">Ride With Us</h3>
-          <button className="partner-button" type="button">
-            Get Started
-          </button>
-        </div>
-      </div>
+      {partnerList.map((partner) => {
+        return (
+          <div
+            className={`partners-card ${partner.className}`}
+            key={partner.id}
+          >
+            <p className="tag">{partner.tag}</p>
+            <div className="content">
+              <p className="tagline">{partner.tagline}</p>
+              <h3 className="heading">{partner.heading}</h3>
+              <button className="partner-button" type="button">
+                {partner.buttonText}
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/utils/partnerList.js
@@ -0,0 +1,20 @@
+const partnerList = [
+  {
+    id: 1,
+    tag: "Earn more with lower fees",
+    tagline: "Signup as a business",
+    heading: "Partner With Us",
+    buttonText: "Get Started",
+    className: "card-partner",
+  },
+  {
+    id: 2,
+    tag: "Avail exclusive perks",
+    tagline: "Signup as a rider",
+    heading: "Ride With Us",
+    buttonText: "Get Started",
+    className: "card-ride",
+  },
+];
+
+export default partnerList;
