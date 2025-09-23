25357c608a8c06f6df61995a0f3bfbbc1a4d05fa
anshu0714 <anshujha14072002@gmail.com>
2025-09-23 23:01:18 +0530

    Add styles to the customer section

--- src/components/sections/CustomerSection.js
+++ src/components/sections/CustomerSection.js
@@ -9,7 +9,7 @@ const CustomerSection = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://randomuser.me/api/?results=5");
+    const data = await fetch("https://randomuser.me/api/?results=8");
     const json = await data.json();
     setCustomersList(json.results);
     console.log(json.results);
@@ -21,18 +21,20 @@ const CustomerSection = () => {
 
   return (
     <div className="customer-section">
-        <h2>Our Happy Customers</h2>
-      {customersList.map((customer) => {
-        return (
-          <div key={customer.login.uuid} className="customer-card">
-            <img src={customer.picture.medium} alt={customer.name.first} />
-            <h4>
-              {customer.name.first} {customer.name.last}
-            </h4>
-            <p>{customer.location.city}</p>
-          </div>
-        );
-      })}
+      <h2 className="customer-section-heading">Loved by Foodies Everywhere 🍔</h2>
+      <div className="customer-containers">
+        {customersList.map((customer) => {
+          return (
+            <div key={customer.login.uuid} className="customer-card">
+              <img src={customer.picture.medium} alt={customer.name.first} />
+              <h4>
+                {customer.name.first} {customer.name.last}
+              </h4>
+              <p>{customer.location.city}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
