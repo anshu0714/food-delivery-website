8777234c520fc83d0b6f2919af6fc2e353b14121
anshu0714 <anshujha14072002@gmail.com>
2025-09-23 23:27:59 +0530

    Fix the grid from 6 to 8 to match customer grids

--- src/components/ui/Shimmer.js
+++ src/components/ui/Shimmer.js
@@ -1,7 +1,17 @@
 const Shimmer = () => {
-  return <div className="shimmer-container">
-    <div className="shimmer-cards">Cards</div>
-  </div>;
+  return (
+    <div className="shimmer-container">
+      {Array(8)
+        .fill("")
+        .map((_, index) => (
+          <div key={index} className="shimmer-card">
+            <div className="shimmer-img"></div>
+            <div className="shimmer-line short"></div>
+            <div className="shimmer-line"></div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Shimmer;
