8f7801e940672055d96edc3557db9647fc864abc
anshu0714 <anshujha14072002@gmail.com>
2025-09-24 13:39:34 +0530

    Toggle login/logout button in header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,34 @@
 import { LOGO_URL } from "../../utils/constants";
+import { useState } from "react";
 
-const Header = () => (
-  <div className="header">
-    <div className="logo-container">
-      <img className="logo" src={LOGO_URL} alt="Food Delivery App Logo" />
+const Header = () => {
+  const [buttonText, setButtonText] = useState("Log In / Sign Up");
+
+  function handleClick() {
+    buttonText == "Log In / Sign Up"
+      ? setButtonText("Log Out")
+      : setButtonText("Log In / Sign Up");
+  }
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} alt="Food Delivery App Logo" />
+      </div>
+      <div className="nav-items-container">
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>Browse Menu</li>
+          <li>Special Offers</li>
+          <li>Restaurants</li>
+          <li>Track Order</li>
+        </ul>
+        <button className="login-btn" type="button" onClick={handleClick}>
+          {buttonText}
+        </button>
+      </div>
     </div>
-    <div className="nav-items-container">
-      <ul className="nav-items">
-        <li>Home</li>
-        <li>Browse Menu</li>
-        <li>Special Offers</li>
-        <li>Restaurants</li>
-        <li>Track Order</li>
-      </ul>
-      <button className="login-btn" type="button">
-        Log In / Sign Up
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
