f44fc78e9fcec583a15a3c905f256290d2aa5e6e
anshu0714 <anshujha14072002@gmail.com>
2025-09-28 21:51:09 +0530

    Add classNames and Home navigation link to ErrorPage

--- src/components/layout/ErrorPage.js
+++ src/components/layout/ErrorPage.js
@@ -1,16 +1,19 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an error occurred.</p>
-      <p>
+    <div className="error-page">
+      <h1 className="error-title">Oops!</h1>
+      <p className="error-message">Sorry, an unexpected error occurred.</p>
+      <p className="error-details">
         <i>{error.statusText || error.message}</i>
       </p>
+      <Link to="/" className="error-home-btn">
+        Go Back Home
+      </Link>
     </div>
   );
 };
