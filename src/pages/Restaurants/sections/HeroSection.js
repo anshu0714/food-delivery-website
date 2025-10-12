ce244d323d00a70c6fbcec204b6431c1d36dd90e
anshu0714 <anshujha14072002@gmail.com>
2025-10-12 23:01:49 +0530

    Add restaurant content and details to HeroSection component

--- src/pages/Restaurants/sections/HeroSection.js
+++ src/pages/Restaurants/sections/HeroSection.js
@@ -1,5 +1,15 @@
 const HeroSection = () => {
-  return <div className="hero-container"></div>;
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <p className="hero-tagline">I'm lovin' it!</p>
+        <h2 className="hero-title">Restaurant Name</h2>
+        <p className="hero-min-order">Minimum Order: 12 GBP</p>
+        <p className="hero-delivery-time">Delivery in 20–25 minutes</p>
+      </div>
+      <p className="hero-open-time">Open until: 3:00 AM</p>
+    </div>
+  );
 };
 
 export default HeroSection;
