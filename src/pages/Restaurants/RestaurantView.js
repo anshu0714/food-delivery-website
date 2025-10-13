daf96bd8b7c9605d8d7a532673e4631500ad28d2
anshu0714 <anshujha14072002@gmail.com>
2025-10-13 18:54:58 +0530

    Fix typo in RestaurantView component: change  to

--- src/pages/Restaurants/RestaurantView.js
+++ src/pages/Restaurants/RestaurantView.js
@@ -1,8 +1,8 @@
 const RestaurantView = () => {
-  returnt(
+  return (
     <div className="res-view">
-      <h2 className="res-view-name">Name of the Restaurant</h2>;
-      <h6 className="menu">Menu0</h6>
+      <h2 className="res-view-name">Name of the Restaurant</h2>
+      <h3 className="menu">Menu</h3>
       <ul className="menu-list">
         <li className="item">Biryani</li>
         <li className="item">Burgers</li>
