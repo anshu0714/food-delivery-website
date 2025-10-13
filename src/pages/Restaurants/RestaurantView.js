d4ad2e48d37e83336c0d6806a75a313ae1649883
anshu0714 <anshujha14072002@gmail.com>
2025-10-13 18:51:36 +0530

    Add RestaurantView component showing restaurant name and sample menu items

---
+++ src/pages/Restaurants/RestaurantView.js
@@ -0,0 +1,15 @@
+const RestaurantView = () => {
+  returnt(
+    <div className="res-view">
+      <h2 className="res-view-name">Name of the Restaurant</h2>;
+      <h6 className="menu">Menu0</h6>
+      <ul className="menu-list">
+        <li className="item">Biryani</li>
+        <li className="item">Burgers</li>
+        <li className="item">Diet Coke</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantView;
