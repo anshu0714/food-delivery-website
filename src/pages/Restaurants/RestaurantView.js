34cfbbfd4a4c4329aeed3f144426ee9626dd3913
anshu0714 <anshujha14072002@gmail.com>
2025-11-13 19:41:28 +0530

    Fetch restaurant details from API and add shimmer loader

--- src/pages/Restaurants/RestaurantView.js
+++ src/pages/Restaurants/RestaurantView.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { RES_API } from "../../utils/apiList";
+import RestaurantCardShimmer from "../../components/ui/RestaurantCardShimmer";
+
 const RestaurantView = () => {
-  return (
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchRestaurantDetails();
+  }, []);
+
+  const fetchRestaurantDetails = async () => {
+    const data = await fetch(RES_API);
+    const json = await data.json();
+    let result =
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants[0]?.info || [];
+
+    setResInfo(result);
+  };
+
+  return resInfo == null ? (
+    <RestaurantCardShimmer />
+  ) : (
     <div className="res-view">
-      <h2 className="res-view-name">Name of the Restaurant</h2>
+      <h2 className="res-view-name">{resInfo.name}</h2>
       <h3 className="menu">Menu</h3>
       <ul className="menu-list">
         <li className="item">Biryani</li>
